171bf55b478be9ff71cc52c11fd5d308c41162cc
Hannes Kindströmmer <me@brolaugh.com>
2017-03-24 15:27:06 +0100

    Add Swedish translation

---
+++ language/sv.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+  'AlnumDashRule' => '{field} måste vara alfabetisk med bindessträck och understräck tillåtna.',
+  'AlnumRule' => '{field} får endast innehålla bokstäver eller nummer.',
+  'AlphaRule' => '{field} får endast innehålla bokstäver.',
+  'ArrayRule' => '{field} måste vara en array.',
+  'BetweenRule' => '{field} måste vara mellan {$0} och {$1}.',
+  'BoolRule' => '{field} måste vara en boolean.',
+  'CheckedRule' => 'You need to check the {field} field.',
+  'DateRule' => '{field} måste vara ett korrekt datum.',
+  'EmailRule' => '{field} måste vara en korrekt emailaddress.',
+  'IntRule' => '{field} måste vara ett heltal.',
+  'IpRule' => '{field} måste vara en korrekt IP address.',
+  'MatchesRule' => '{field} måste matcha {$0}.',
+  'MaxRule' => '{field} får som störst vara {$0}.',
+  'MinRule' => '{field} får som minst vara {$0}.',
+  'NumberRule' => '{field} måste vara ett nummer.',
+  'RegexRule' => '{field} var inte i rätt format.',
+  'RequiredRule' => '{field} är obligatorisk.',
+  'UrlRule' => '{field} måste vara en korrekt URL.'
+);
